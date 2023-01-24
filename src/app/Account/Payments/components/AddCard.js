5d382db6bba1df8ad8dd13295efa4186c7c813be
Okraks <antwisaac7@gmail.com>
2023-01-24 19:28:14 +0000

    payment card add

--- src/app/Account/Payments/components/AddCard.js
+++ src/app/Account/Payments/components/AddCard.js
@@ -1,9 +1,60 @@
 import React from "react";
 
+// payment methods
+import amex from "../../../../assets/icons/payment/amex.svg";
+import visa from "../../../../assets/icons/payment/visa.svg";
+import mastercard from "../../../../assets/icons/payment/mastercard.svg";
+
 function AddCard() {
   return (
     <div>
-      <div className="my-20">Card Details</div>
+      {/* payment methods */}
+      <div className="flex items-center gap-4">
+        <img src={visa} alt="visa" />
+        <img src={amex} alt="amex" />
+        <img src={mastercard} alt="mastercard" />
+      </div>
+
+      {/* Other Details */}
+      <div className="text-gray font-light border rounded-xl border-mute my-5">
+        <input
+          className="w-full my-3 pl-5 mt-2 focus:outline-none outline-none"
+          placeholder="Card Number"
+          type="text"
+        />
+
+        <div className="text-mute block">
+          <hr className="" />
+        </div>
+        <div className="grid text-left grid-cols-2 divide-x divide-mute">
+          <div>
+            <input
+              className="pl-5 my-3"
+              type=" text"
+              placeholder="Expiry date"
+            />
+          </div>
+          <div>
+            <input className="pl-5 my-3" type="text" placeholder="CVV" />
+          </div>
+        </div>
+      </div>
+
+      {/* Postal Code */}
+      <input
+        className="form-input mb-3"
+        type="text"
+        placeholder="Postal Code"
+      />
+
+      {/* Country */}
+      <input
+        className="form-input mb-3"
+        type="text"
+        placeholder="Country / Region"
+      />
+
+      {/* Button to add Card */}
       <button className="btn-primary w-full"> Add Card</button>
     </div>
   );

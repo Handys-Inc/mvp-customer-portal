d16bdd4f84c70c26526ce4c2ec0ce6cc1e16a9e3
Okraks <antwisaac7@gmail.com>
2023-01-27 18:49:01 +0000

    main payments scrollable issue fix

--- src/app/Account/Payments/Payments.js
+++ src/app/Account/Payments/Payments.js
@@ -19,7 +19,7 @@ function Payments() {
       <Header />
       <AccountHeader title="Payments & Payouts" />
 
-      <div className="flex flex-col md:flex-row gap-32 items-start justify-between  max-w-5xl mx-auto mb-20">
+      <div className="flex flex-col md:flex-row gap-32 items-start justify-between  max-w-5xl mx-auto mb-20 min-h-screen">
         <div className="flex-1 min-h-[20rem]">
           <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
             <div class="border-b border-mute">

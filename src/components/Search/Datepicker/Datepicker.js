2867a03b83e20afeedb9bb69692f68363204a69e
Okraks <antwisaac7@gmail.com>
2023-02-02 15:30:38 +0000

    React DayPicker Integration Complete

---
+++ src/components/Search/Datepicker/Datepicker.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { DayPicker } from "react-day-picker";
+
+import "react-day-picker/dist/style.css";
+import "./index.css";
+
+export default function Datepicker({ selected, setSelected }) {
+  return (
+    <DayPicker
+      className="text-xs mx-auto inline-block"
+      mode="range"
+      numberOfMonths={2}
+      selected={selected}
+      onSelect={setSelected}
+    />
+  );
+}

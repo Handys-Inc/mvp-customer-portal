7e01154a1ad9088c93d48d8040a57ee8e0533745
Okraks <antwisaac7@gmail.com>
2023-02-03 16:49:15 +0000

    Mobile search

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Selector from "./components/Selector";
 import Services from "./components/Services";
 import Layout from "../../components/Layout/Layout";
+
+import { FiSearch } from "react-icons/fi";
+import { BsSliders } from "react-icons/bs";
+import FullModal from "../../components/FullModal/FullModal";
+import MobileSearch from "../MobileSearch/MobileSearch";
 
 function Home() {
   // scroll to top
@@ -9,9 +14,38 @@ function Home() {
     window.scrollTo(0, 0);
   }, []);
 
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+
   return (
     <div>
       <Layout>
+        {/* MOBILE SEARCH TOGGLE */}
+        <div
+          onClick={() => setShowMobileSearch(true)}
+          className="md:hidden mt-10 items-center justify-between  flex border-faintGray border shadow-md rounded-full py-2 px-4"
+        >
+          <div className="flex gap-4 items-center">
+            {" "}
+            <FiSearch className="text-gray" size={20} />
+            <div>
+              <p className="font-semibold text-sm">What's needed?</p>
+              <p className="text-gray text-xs">Where . When . How Long</p>
+            </div>
+          </div>
+          <div>
+            <BsSliders />
+          </div>
+        </div>
+
+        {/* MODAL FOR Mobile Search */}
+        <FullModal
+          isOpen={showMobileSearch}
+          setIsOpen={setShowMobileSearch}
+          title="Search"
+        >
+          <MobileSearch />
+        </FullModal>
+
         <Selector />
 
         <Services />
--- src/components/Search/Datepicker/Datepicker.js
+++ src/components/Search/Datepicker/Datepicker.js
@@ -5,12 +5,12 @@ import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import "./index.css";
 
-export default function Datepicker({ selected, setSelected }) {
+export default function Datepicker({ number = 2, selected, setSelected }) {
   return (
     <DayPicker
       className="text-sm mx-auto inline-block"
       mode="range"
-      numberOfMonths={2}
+      numberOfMonths={number}
       selected={selected}
       onSelect={setSelected}
     />

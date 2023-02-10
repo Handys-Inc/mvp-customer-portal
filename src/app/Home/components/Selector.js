699d1eda75fc9fbfb29ac7c9d9616afc64d48007
Okraks <antwisaac7@gmail.com>
2023-02-10 17:55:01 +0000

    selector service color and horizontal fix

--- src/app/Home/components/Selector.js
+++ src/app/Home/components/Selector.js
@@ -11,7 +11,7 @@ function Selector() {
   const selectors = [
     {
       name: "Painter",
-      icon: <Roller size={18} className="selector-icon" />,
+      icon:  <Roller  size={18} className="selector-icon" />
     },
     {
       name: "Plumber",
@@ -19,7 +19,7 @@ function Selector() {
     },
     {
       name: "Electrician",
-      icon: <Bulb size={18} className="selector-icon" />,
+      icon: <Bulb  size={18} className="selector-icon" />,
     },
     {
       name: "Cleaner",
@@ -30,24 +30,63 @@ function Selector() {
       icon: <Worker size={18} className="selector-icon" />,
     },
   ];
+
+    const mobileSelectors = [
+    {
+      name: "Painter",
+      icon:  <Roller fill="#CE1449" size={18} className="selector-icon" />
+    },
+    {
+      name: "Plumber",
+      icon: <Wrench fill="#CE1449" size={18} className="selector-icon" />,
+    },
+    {
+      name: "Electrician",
+      icon: <Bulb fill="#CE1449" size={18} className="selector-icon" />,
+    },
+    {
+      name: "Cleaner",
+      icon: <Broom fill="#CE1449" size={18} className="selector-icon" />,
+    },
+    {
+      name: "General handy person",
+      icon: <Worker fill="#CE1449" size={18} className="selector-icon" />,
+    },
+  ];
   return (
-    <Fragment className="">
-      <div className="w-full overflow-x-auto flex mt-5 flex-row items-center space-x-5 justify-center mb-5">
-        {selectors.map((selector) => {
-          return (
-            <div className="selector-container">
-              {selector.icon}
+<Fragment>
+  {/* desktop selectors */}
+<div className="hidden md:flex tab-scroll-none md:justify-center  overflow-x-auto space-x-5 flex-nowrap mt-4 my-2">
+      {selectors.map((selector) => {
+        return (
+          <div className="selector-container">
+            {selector.icon}
 
-              <p className="ml-0 mt-2 md:mt-0 md:ml-2">{selector.name}</p>
-            </div>
-          );
-        })}
-      </div>
+            <p className="whitespace-nowrap ml-0 mt-2 md:mt-0 md:ml-2 ">
+              {selector.name}
+            </p>
+          </div>
+        );
+      })}
+    </div>
 
-      <div className="text-faintGray mb-5">
-        <hr />
-      </div>
-    </Fragment>
+    {/* mobile selectors */}
+    <div className="md:hidden flex tab-scroll-none md:justify-center  overflow-x-auto space-x-5 flex-nowrap mt-4 my-2">
+      {mobileSelectors.map((selector) => {
+        return (
+          <div className="selector-container">
+            {selector.icon}
+
+            <p className="whitespace-nowrap ml-0 mt-2 md:mt-0 md:ml-2 ">
+              {selector.name}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+</Fragment>
+    
+    
   );
 }
 
